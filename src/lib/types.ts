dd94b2b757b679f30290ce69df5f4cf76fbce284
Martin Tricaud <martin.tricaud@gmail.com>
2023-01-09 10:15:54 +0100

    code cleanup

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,19 +10,6 @@ export interface PresetV{
 export interface Preset extends PresetV{
     name:string
 }
-export interface PresetV2{
-    h_local2: Record<string, string>, h_global2:Record<string, string>, ranges: Array<[string, Record<string, number>]>, z:number
-}
-export interface Preset2 extends PresetV2{
-    name:string
-}
-
-export interface PresetV3{
-    macro: Array<[string, Record<string, string>]>, ranges: Array<[string, Record<string, number>]>, z:number
-}
-export interface Preset3 extends PresetV3{
-    name:string
-}
 export enum Constraints {
     Outisde,
     Within,
