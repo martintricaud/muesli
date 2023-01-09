9d4e5fd6825e0bfe284037d7fec1b5d45133f608
Martin Tricaud <martin.tricaud@gmail.com>
2023-01-09 16:10:36 +0100

    refactored the constraint solver to make it less hard coded (i.e constraints are passed as arguments)

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,19 @@
 <script lang ts>
-  import { writable, get} from 'svelte/store';
+  import { writable, get } from 'svelte/store';
   import { onMount } from 'svelte';
-  import { random_adjunction, MuesliStore, liftSolve, solve, PresetStore } from './lib/stores';
-  import {
-    shiftdown_,
-    shiftup_,
-    mouseup_,
-    mousedown_,
-    mousemove_,
-    mousewheel_,
-    asr,
-  } from './lib/utils-streams';
+  import { random_adjunction, MuesliStore, liftSolve, solve, PresetStore, solve3, solve4, constraintsPreset } from './lib/stores';
+  import { mousewheel_ } from './lib/utils-streams';
   import { synth2, preset2, preset2bis } from './lib/data';
   import { wasm_functions as W } from './main.js';
   import * as U from './lib/utils';
   import * as S from './lib/utils-streams';
   import * as R from 'ramda';
-  import * as K from 'kefir';
   import HydraViewer from './lib/HydraViewer.svelte';
   import IGrab from './lib/IGrab.svelte';
   import ICursor from './lib/ICursor.svelte';
   import Info from './lib/Info.svelte';
   import ISet from './lib/ISet.svelte';
+  import IFix from './lib/IFix.svelte';
   import { prng_alea } from 'esm-seedrandom';
 
   /*
@@ -47,12 +39,21 @@
   let noscroll;
 
   let instruments = {
-    fix: {
-      name: 'fix',
+    eraser2: {
+      name: 'erase',
       equipped: false,
       component: Info,
       ev: { foo: null },
+      ff: 'willErase',
+      effect: R.omit,
+    },
+    fix: {
+      name: 'fix',
+      equipped: false,
+      component: IFix,
+      ev: { foo: null },
       ff: 'willFix',
+      effect: fix_effect,
     },
     eraser: {
       name: 'erase',
@@ -102,8 +103,8 @@
       z: 1,
     },
   ]);
-  $: preset_stores = R.map(x=>MuesliStore(x)[2],$presets)
-  $: preset_data = R.map(x=>R.pluck('a',get(x)),preset_stores)
+
+  $: preset_data = R.map((x) => R.pluck('a', get(MuesliStore(x)[2])), $presets);
   //presetFocus defines the preset of parameters currently loaded
   //zf is the zoom factor
   const [presetFocus, zf] = [writable(0), writable(1)];
@@ -113,26 +114,33 @@
   $: data_a = R.pluck('a', $P);
 
   // STREAMS AND THEIR OBSERVERS
-  let  wheel_, move_, effect;
-  wheel_ = move_ = effect = undefined;
+  let wheel_, effect;
+  wheel_ = effect = undefined;
 
-  function set_effect(ev){
-    console.log(ev)
+  // EFFECTFUL FUNCTIONS - modify reactive values, can be thought of has handlers
+
+  function set_effect(ev) {
+    console.log(ev);
+  }
+
+  function fix_effect(ev) {
+    //if a param is fixed, its update function is an empty object
+    //fixing a param means saving it in a preset, and mergeDeepWithKey 
+    console.log(ev);
   }
 
   function displace_effect(ev) {
     let e = ev.detail;
-  
     if (e.atk?.target?.classList?.contains('displacable')) {
       const store = e.atk.target.dataset?.store;
       const [key, field] = U.stringToPath(e.atk.target.dataset.path);
       let f;
       if (store == 0 || store == 1) {
-        f = U.add(U.scale(0, track_w, '0', U.fMAX_H($B, 19), e.movementX));
+        f = U.add(U.scale(0, track_w, '0', U.fMAX_H($B, $Keys.length), e.movementX));
       }
       if (store == 2) {
         let f0 = U.add(U.scale(0, track_w, $P[key].c0, $P[key].c1, e.movementX));
-        f = liftSolve(solve, U.deepObjOf([key, field], f0));
+        f = liftSolve(solve4(constraintsPreset), U.deepObjOf([key, field], f0));
       }
       muesli?.[store]?.update(f);
     }
@@ -142,8 +150,6 @@
     presets.add(val);
   }
 
-  // EFFECTFUL FUNCTIONS - modify reactive values, can be thought of has handlers
-
   function equip_effect(name) {
     instruments = R.mapObjIndexed(
       (x, k) => R.modify('equipped', k == name ? R.T : R.F, x),
@@ -151,7 +157,6 @@
     );
   }
 
- 
   function click_effect(ev) {
     let target = ev.target;
     if (equipped == 'eraser') {
@@ -172,35 +177,32 @@
   function wheel_effect(e) {
     let curr = R.clamp(0.0000001, 1, $zf + U.scale(0, ih * 100, 0.0000001, 1, e.wheelDeltaY));
     let f = (x) => (curr / $zf) * x;
-    muesli[2].update(liftSolve(solve, R.map(R.always(R.objOf('z', f)), $P)));
+    muesli[2].update(liftSolve(solve4(constraintsPreset), R.map(R.always(R.objOf('z', f)), $P)));
     zf.set(curr);
   }
 
   onMount(() => {
     //pass device events as inputs to the equipped instrument, which acts as a transfer function
-    function feedback(e){ instruments[equipped] = R.assoc('ev', e, instruments[equipped])}
-    S.keydown_(' ').thru(S.obs((x) => (hidden = true)));
-    S.keyup_(' ').thru(S.obs((x) => (hidden = false)));
+    function feedback(e) {
+      instruments[equipped] = R.assoc('ev', e, instruments[equipped]);
+    }
     S.mousemove_.thru(S.obs(feedback));
     S.mousedown_.thru(S.obs(feedback));
     S.drag_.thru(S.obs(feedback));
-    wheel_ = S.asr(S.capture($zf, shiftdown_), mousewheel_, shiftup_).thru(S.obs(wheel_effect));
+    wheel_ = S.asr(S.capture($zf, S.shiftdown_), mousewheel_, S.shiftup_).thru(S.obs(wheel_effect));
   });
 </script>
 
-<svelte:window 
-  bind:innerHeight={ih} 
-  bind:innerWidth={iw} 
-  on:keydown={ev=>noscroll = ev.shiftKey} 
-  on:keyup={ev=>noscroll = ev.shiftKey}
+<svelte:window
+  bind:innerHeight={ih}
+  bind:innerWidth={iw}
+  on:keydown={(ev) => (noscroll = ev.shiftKey)}
+  on:keyup={(ev) => (noscroll = ev.shiftKey)}
+  on:keydown={(ev) => (hidden = ev.key == ' ')}
+  on:keyup={(ev) => (hidden = ev.key != ' ')}
 />
 <main class="grid">
-  <div
-    class:hidden={!hidden}
-    class="instruments"
-    style="left:100px; top:0px; z-index:1000"
-    bind:clientWidth={fooWidth}
-  >
+  <div class:hidden={!hidden} class="instruments" style="left:100px; top:0px; z-index:1000">
     {#each Object.entries(instruments) as [name, { equipped }]}
       <div class:equipped on:mouseenter={() => equip_effect(name)}>{name}</div>
     {/each}
@@ -210,22 +212,28 @@
   {/each}
   <div class="menu">
     <button
-        data-path=""
-        on:click={() =>
-          save_effect({ ranges: data, h_global: $H_global, h_local: $H_local, z: $zf })}
-        >save</button
-      >
-      {#if $presets.length > 0}
-        {#each $presets as { name }, i}
-          <div class="preset erasable {ff}" on:click={() => load_effect(i)}>
-            <HydraViewer synth={synth2} data={preset_data[i]} w={160} h={100} autoloop={false} size={"thumbnail"}></HydraViewer>
-            <span>{name}</span>
-          </div>
-        {/each}
-      {/if}
+      data-path=""
+      on:click={() => save_effect({ ranges: data, h_global: $H_global, h_local: $H_local, z: $zf })}
+      >save</button
+    >
+    {#if $presets.length > 0}
+      {#each $presets as { name }, i}
+        <div class="preset erasable {ff}" on:click={() => load_effect(i)}>
+          <HydraViewer
+            synth={synth2}
+            data={preset_data[i]}
+            w={160}
+            h={100}
+            autoloop={false}
+            size={'thumbnail'}
+          />
+          <span>{name}</span>
+        </div>
+      {/each}
+    {/if}
   </div>
   <div class="ui">
-    <ul class="slider-container">
+    <ul class="sliders">
       <li class="macro">
         <div class="param-name">Global macro</div>
         <div class="track" bind:clientWidth={macro_w}>
@@ -233,7 +241,7 @@
             class="thumb displacable"
             data-store="0"
             data-path="h b"
-            style="left:{W.rescale_index($H_global, $B, 19) * macro_w}px"
+            style="left:{W.rescale_index($H_global, $B, $Keys.length) * macro_w}px"
             bind:clientWidth={thumb_w}
           />
         </div>
@@ -245,21 +253,16 @@
             data-store="1"
             data-path="h a"
             class="thumb displacable"
-            style="left:{W.rescale_index($H_local, $B, 19) * macro_w}px"
+            style="left:{W.rescale_index($H_local, $B, $Keys.length) * macro_w}px"
             bind:clientWidth={thumb_w}
           />
         </div>
       </li>
     </ul>
-    <ul class="slider-container" id="all-sliders" style="overflow:{noscroll?'hidden':'scroll'}">
+    <ul class="sliders" style="overflow:{noscroll ? 'hidden' : 'scroll'}">
       <!-- The list of individual sliders is generated by iterating over the store containing sliders data-->
       {#each Object.entries($P) as [key, { a, b, c0, c1, z }]}
-        <li
-          class="erasable {ff}"
-          on:click={click_effect}
-          data-path={key}
-          data-store="2"
-        >
+        <li class="erasable {ff}" on:click={click_effect} data-path={key} data-store="2">
           <div class="param-name {ff}" data-store="2">{key}</div>
           <div class="bound c0">{c0}</div>
           <div class="track" bind:clientWidth={track_w}>
@@ -283,8 +286,7 @@
           <div class="bound c1">{c1}</div>
         </li>
       {/each}
-      </ul>
-  
+    </ul>
   </div>
   <div class="viewport">
     <!-- <HydraViewer synth={synth2} data={data_a} w={1600} h={1000} autoloop={true} size={"big"}/> -->
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -16,7 +16,7 @@ let randomForward = axes => _n => R.zipObj(
 	axes,
 	axes.map(
 		(_val) =>
-			U.lerp(0, 1, 0, Math.pow(2, 32) - 1,Math.random())
+			U.lerp(0, 1, 0, Math.pow(2, 32) - 1, Math.random())
 	)
 )
 
@@ -26,7 +26,7 @@ function randomReverse(X) {
 	let dim = R.values(a).length
 	let max = W.max_hilbert(32, dim)
 	let rand = prng_alea(b)
-	let res = U.scale2bigint(0, 1,0, max,rand)
+	let res = U.scale2bigint(0, 1, 0, max, rand)
 	return res
 }
 export const random_adjunction = (axes) => [
@@ -66,57 +66,144 @@ export function sync(left, right) {
 	];
 }
 
+
+
+const getEvolverWithConstraints = (obj, evolver, constraints) =>
+	R.reduce(
+		(acc, { predicate, g }) =>
+			R.cond([
+				[predicate, R.always(acc)],
+				[R.T, R.always(R.compose(acc, g))]
+			])(obj),
+		evolver,
+		constraints
+	);
+
+
+
 // Hard coded simplex solver that maintains constraints between fields a, b, c0, c1, z of an object
-export function solve(update: P<E<num>>, param: P<num>) {
+export function solve(update: P<E<num>>, param: P<num>, locked = false) {
+	const willStayUnder = (target, bound, tolerance = 0) => (bound - target) >= tolerance
+	const willStayBetween = (target, [m, M]) => willStayUnder(m, target[0]) && willStayUnder(R.last(target), M)
 	let e = 0.01 //tolerance
 	const fields: str[] = ["a", "z", "b", "c0", "c1"]
 	const [fa, fz, fb, fc0, fc1]: E<num>[] = R.map((x: E<num>) => x ??= R.identity, R.props(fields, update)) //fields that are not updated are updated with the identity
 	const [va, vz, vb, vc0, vc1]: num[] = R.props(fields, param)
 	let gz, gb, ga, gc0, gc1
-	[gc0, gc1] = U.willStayUnder(e, fc1(vc1) - fc0(vc0)) ? [fc0, fc1] : [x => fc1(x - e / 2) - e / 2, x => fc0(x + e / 2) + e / 2]
-	let boundsOfZ:[num, num] = [0,gc1(vc1) - gc0(vc0)]
-	gz = U.willStayBetween([fz(vz)], boundsOfZ) ? fz : R.pipe(fz,R.clamp(...boundsOfZ))
+	[gc0, gc1] = willStayUnder(e, fc1(vc1) - fc0(vc0)) ? [fc0, fc1] : [x => fc1(x - e / 2) - e / 2, x => fc0(x + e / 2) + e / 2]
+	let boundsOfZ: [num, num] = [0, gc1(vc1) - gc0(vc0)]
+	gz = willStayBetween([fz(vz)], boundsOfZ) ? fz : R.pipe(fz, R.clamp(...boundsOfZ))
 	let boundsOfB: [num, num] = [gc0(vc0) + gz(vz) / 2, gc1(vc1) - gz(vz) / 2]
-	gb = U.willStayBetween([fb(vb)], boundsOfB) ? fb : R.pipe(fb,R.clamp(...boundsOfB))
+	gb = willStayBetween([fb(vb)], boundsOfB) ? fb : R.pipe(fb, R.clamp(...boundsOfB))
 	let boundsOfA: [num, num] = [gb(vb) - gz(vz) / 2, gb(vb) + gz(vz) / 2]
-	ga = U.willStayBetween([fa(va)], boundsOfA) ? fa : R.pipe(fa,R.clamp(...boundsOfA))
+	ga = willStayBetween([fa(va)], boundsOfA) ? fa : R.pipe(fa, R.clamp(...boundsOfA))
 	let res: P<E<num>> = { a: ga, z: gz, b: gb, c0: gc0, c1: gc1 }
 	return res
 }
 
+const composeEvolver = (evolver1, evolver2) => R.mergeWith((f,g)=>R.compose(f,g),evolver1, evolver2)
+const pipeEvolver = (evolver1, evolver2) => R.mergeWith((f,g)=>R.pipe(f,g),evolver1, evolver2)
+
+const getEvolverIfViolatesPredicate = (obj, evolver, predicate, g) =>
+	R.cond([
+		[predicate, R.always(evolver)],
+		[R.T, R.always(pipeEvolver(evolver, g))]
+	])(obj);
+
+export function solve3(update: P<E<num>>, v: P<num>, locked = false) {
+	let e = 0 // tolerance
+	const g0: P<E<num>> = R.mapObjIndexed((x: num,i) => update[i]== undefined ? R.identity: update[i], v) // fields that are not updated are updated with the identity
+	let g1 = getEvolverIfViolatesPredicate(v, g0, obj => obj.c1 - obj.c0 >=e, {
+		c0: x => g0.c1(x - e / 2) - e / 2,
+		c1: x => g0.c0(x + e / 2) + e / 2
+	})
+	let g2 = getEvolverIfViolatesPredicate(v, g1, obj => obj.z <= obj.c1 - obj.c0 && obj.z >= e, {
+		z: R.clamp(e, g1.c0(v.c0)-g1.c1(v.c1))
+	})
+	let g3 = getEvolverIfViolatesPredicate(v, g2, obj => obj.b >= obj.c0+obj.z/2 && obj.b <=  obj.c1-obj.z/2, {
+		b: R.clamp(g2.c0(v.c0)+g2.z(v.z)/2, g2.c1(v.c1)-g2.z(v.z)/2)
+	})
+	let g4 = getEvolverIfViolatesPredicate(v, g3, obj => obj.a >= obj.b - obj.z / 2 && obj.a <= obj.b + obj.z / 2, {
+		a: R.clamp(g3.b(v.b) - g3.z(v.z) / 2, g3.b(v.b) + g3.z(v.z) / 2)
+	})
+	return g4
+}
+
+let e = 0.01
+
+export const solve4 = constraints => (update: P<E<num>>, v: P<num>) => {
+	const g0: P<E<num>> = R.mapObjIndexed((x: num,i) => update[i]== undefined ? R.identity: update[i], v) // fields that are not updated are updated with the identity
+	return constraints.reduce((evolver, {predicate, g}) => getEvolverIfViolatesPredicate(v, evolver, predicate, g(evolver, v)), g0)
+}
+
+export const constraintsPreset = [
+	{
+		predicate: obj => obj.c1 - obj.c0 >= e,
+		g: (f,v) => {
+			return {
+				c0: x => f.c1(x - e / 2) - e / 2,
+				c1: x => f.c0(x + e / 2) + e / 2
+			}
+		}
+	},
+	{
+		predicate: obj => obj.z <= obj.c1 - obj.c0 && obj.z >= e,
+		g: (f,v) => {
+			return {
+				z: R.clamp(e, f.c0(v.c0)-f.c1(v.c1))
+			}
+		}
+	},
+	{
+		predicate: obj => obj.b >= obj.c0+obj.z/2 && obj.b <=  obj.c1-obj.z/2,
+		g: (f,v) => {
+			return {
+				b: R.clamp(f.c0(v.c0)+f.z(v.z)/2, f.c1(v.c1)-f.z(v.z)/2)
+			}
+		}
+	},
+	{
+		predicate: obj => obj.a >= obj.b - obj.z / 2 && obj.a <= obj.b + obj.z / 2, 
+		g: (f,v) => {
+			return {
+				a: R.clamp(f.b(v.b) - f.z(v.z) / 2, f.b(v.b) + f.z(v.z) / 2)
+			}
+		}
+	}
+]
 // lifts a solver to array of objects
-export const liftSolve = (solver: T_Solver, F: P<P<E<num>>>) => (X: P<P<num>>) => R.evolve(U.mergePartialWith(solver, F, X),X)
+export const liftSolve = (solver: T_Solver, F: P<P<E<num>>>) => (X: P<P<num>>) => R.evolve(U.mergePartialWith(solver, F, X), X)
 
 
 export function liftedConstraintStore(ranges: Array<[str, P<any>]>, _epsilon: num = 0.01) {
 	//compute a default value for a parameter
-	function computeDefault(p){
+	function computeDefault(p) {
 		let r = p;
-		r.z??=p.c1-p.c0;
-		r.b??= (p.c1-p.c0)/2
-		r.a??= U.lerp(0,1,r.b-r.z/2, r.b+r.z/2,Math.random())
+		r.z ??= p.c1 - p.c0;
+		r.b ??= (p.c1 - p.c0) / 2
+		r.a ??= U.lerp(0, 1, r.b - r.z / 2, r.b + r.z / 2, Math.random())
 		return r
 	}
 	//initialize object with default args in each field.
 	let dataObj: P<P<num>> = R.fromPairs(ranges)
 	let X0: P<P<num>> = R.map(computeDefault, dataObj)
 	let F0 = U.deepMap(R.always, dataObj)
-	const Params: Writable<P<P<num>>> = writable(liftSolve(solve,F0)(X0)) //store of records
+	const Params: Writable<P<P<num>>> = writable(liftSolve(solve4(constraintsPreset), F0)(X0)) //store of records
 	const Keys: Writable<str[]> = writable(R.map(x => x[0], ranges))
 
 	function set_S(X: P<P<num>>) {
-		Params.update(liftSolve(solve, U.deepMap(R.always, X)))
+		Params.update(liftSolve(solve4(constraintsPreset), U.deepMap(R.always, X)))
 	}
 
 	return [
-		{ subscribe: Params.subscribe, set: set_S, update: Params.update},
+		{ subscribe: Params.subscribe, set: set_S, update: Params.update },
 		{ subscribe: Keys.subscribe, set: Keys.set, update: Keys.update }
 	]
 }
 
 export function MuesliStore(data) {
 	// B = bits per dimensions, P = parameters, K = Keys, HF = Hilbert Forward, HR = Hilbert Reverse
-	const [Params, Keys, Bits] = [...liftedConstraintStore(data.ranges),writable(32)]
+	const [Params, Keys, Bits] = [...liftedConstraintStore(data.ranges), writable(32)]
 	let $Params, $Keys, $Bits
 	Bits.subscribe(b => { $Bits = b })
 	Params.subscribe(p => { $Params = p; })
@@ -127,26 +214,26 @@ export function MuesliStore(data) {
 	const [H_local, H_global] = [writable(data.h_local), writable(data.h_global)]
 
 	let lensA = U.lerpLens('a', obj => [obj.b - obj.z / 2, obj.b + obj.z / 2], [0, U.fMAX_A($Bits)])
-	let lensB = U.lerpLens('b', obj => [obj.c0, obj.c1], [0, U.fMAX_A($Bits)])
+	let lensB = U.lerpLens('b', obj => [obj.c0+ obj.z / 2, obj.c1- obj.z / 2], [0, U.fMAX_A($Bits)])
 
 	function setH_local(h: string) {
-		updateH_local(R.always(U.clamp('0', U.fMAX_H($Bits, $Keys.length),h)))
+		updateH_local(R.always(U.clamp('0', U.fMAX_H($Bits, $Keys.length), h)))
 	}
 
 	function setH_global(h: string) {
-		updateH_global(R.always(U.clamp('0', U.fMAX_H($Bits, $Keys.length),h)))
+		updateH_global(R.always(U.clamp('0', U.fMAX_H($Bits, $Keys.length), h)))
 	}
 
-	
+
 	function updateH_local(f_h) {
-		H_local.update(x => U.clamp('0', U.fMAX_H($Bits, $Keys.length),f_h(x)))
-		let hx = R.zipObj($Keys, W.forward(get(H_local),$Bits,$Keys.length))
+		H_local.update(x => U.clamp('0', U.fMAX_H($Bits, $Keys.length), f_h(x)))
+		let hx = R.zipObj($Keys, W.forward(get(H_local), $Bits, $Keys.length))
 		Params.set(R.mergeWith(R.set(lensA), hx, $Params))
 	}
-	
+
 	function updateH_global(f_h) {
-		H_global.update(x => U.clamp('0', U.fMAX_H($Bits, $Keys.length),f_h(x)))
-		let hx: P<num> = R.zipObj($Keys, W.forward(get(H_global),$Bits,$Keys.length))
+		H_global.update(x => U.clamp('0', U.fMAX_H($Bits, $Keys.length), f_h(x)))
+		let hx: P<num> = R.zipObj($Keys, W.forward(get(H_global), $Bits, $Keys.length))
 		Params.set(R.mergeWith(R.set(lensB), hx, $Params))
 	}
 
@@ -156,12 +243,12 @@ export function MuesliStore(data) {
 
 	function updateParams(X) {
 		Params.update(X)
-		let [H_global_setters,H_local_setters] = [
+		let [H_global_setters, H_local_setters] = [
 			R.mapObjIndexed(R.view(lensB), $Params),
 			R.mapObjIndexed(R.view(lensA), $Params)
 		]
-		H_local.set(W.inverse(u32a.from(R.props($Keys,H_local_setters)), $Bits))
-		H_global.set(W.inverse(u32a.from(R.props($Keys,H_global_setters)), $Bits))
+		H_local.set(W.inverse(u32a.from(R.props($Keys, H_local_setters)), $Bits))
+		H_global.set(W.inverse(u32a.from(R.props($Keys, H_global_setters)), $Bits))
 	}
 	return [
 		{ subscribe: H_global.subscribe, set: setH_global, update: updateH_global },
@@ -172,12 +259,12 @@ export function MuesliStore(data) {
 	]
 }
 
-export function PresetStore(init: T.Preset[]|[]) {
-	const Presets: Writable<T.Preset[]|[]> = writable(init);
+export function PresetStore(init: T.Preset[] | []) {
+	const Presets: Writable<T.Preset[] | []> = writable(init);
 
 	function addPreset(v: T.Preset, i: num = 10) {
 		let f = (newVal, id = 10) => (presets: T.Preset[]) => R.insert(
-			id, { ...{ name: U.assignDefaultName(R.pluck('name', presets)) }, ...newVal,}, presets
+			id, { ...{ name: U.assignDefaultName(R.pluck('name', presets)) }, ...newVal, }, presets
 		);
 		Presets.update(f(v, i))
 	}
@@ -191,6 +278,6 @@ export function PresetStore(init: T.Preset[]|[]) {
 		//call update with a deepMerge
 	}
 
-	return { subscribe: Presets.subscribe, set: Presets.set, update: Presets.update, erase: deletePreset, modify: modifyPreset, add:addPreset}
-	
+	return { subscribe: Presets.subscribe, set: Presets.set, update: Presets.update, erase: deletePreset, modify: modifyPreset, add: addPreset }
+
 }
